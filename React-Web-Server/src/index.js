cbcbc7a460f70e2b6a3b78cc92a9343615cf7303
Arthur <arthur.tristram1@gmail.com>
2025-07-28 15:49:09 -0400

    Construction Cognito authority from UPID and region

--- React-Web-Server/src/index.js
+++ React-Web-Server/src/index.js
@@ -5,8 +5,12 @@ import { AuthProvider } from "react-oidc-context";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
+// Extract region from User Pool ID (e.g., 'us-east-1_xxx' -> 'us-east-1')
+const userPoolId = process.env.REACT_APP_COGNITO_USER_POOL_ID;
+const region = userPoolId?.split('_')[0] || 'us-east-1';
+
 const cognitoAuthConfig = {
-  authority: process.env.REACT_APP_COGNITO_AUTHORITY,
+  authority: `https://cognito-idp.${region}.amazonaws.com/${userPoolId}`,
   client_id: process.env.REACT_APP_COGNITO_CLIENT_ID,
   redirect_uri: process.env.REACT_APP_COGNITO_REDIRECT_URI,
   response_type: "code",
